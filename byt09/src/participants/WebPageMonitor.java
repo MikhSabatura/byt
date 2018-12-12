f999ef4ff70f4a488ab03d0fa1b0c31f1df52607
MikhSabatura <mikhailsabatura@gmail.com>
2018-12-12 19:00:10 +0100

    added javaDoc

--- byt09/src/participants/WebPageMonitor.java
+++ byt09/src/participants/WebPageMonitor.java
@@ -18,6 +18,9 @@ public class WebPageMonitor {
         _observers = new HashMap<>();
     }
 
+    /**
+     * regularly updates information about the modification dates
+     */
     public void monitor() {
         while (true) {
             try {
@@ -29,6 +32,11 @@ public class WebPageMonitor {
         }
     }
 
+    /**
+     * updates last modified date for the passed url and notifies attached observers
+     *
+     * @param url url for which last modified date will be updated
+     */
     private void updateLastModified(URL url) {
         Date currLastModified = null;
         try {
@@ -42,14 +50,27 @@ public class WebPageMonitor {
         }
     }
 
+    /**
+     * @param url url for which last modified date will be checked
+     * @return date when the page under passed url was modified
+     */
     private Date getServerLastModified(URL url) throws IOException {
         return new Date(url.openConnection().getLastModified());
     }
 
+    /**
+     * @param url             url observers of which will be notified
+     * @param newLastModified new date of last modification that will be passed to observers
+     */
     private void notifyObservers(URL url, Date newLastModified) {
         _observers.get(url).forEach(o -> o.update(newLastModified));
     }
 
+    /**
+     * adds the url to the set of monitored in case it isn't already
+     *
+     * @param url url of the page which will be added to the set of monitored
+     */
     public void addMonitoredPage(URL url) {
         try {
             _lastModified.putIfAbsent(url, getServerLastModified(url));
@@ -63,12 +84,20 @@ public class WebPageMonitor {
         _observers.remove(url);
     }
 
+    /**
+     * @param url      url to which the observer will be attached
+     * @param observer observer that will be attached to the url
+     */
     public void addObserver(URL url, IObserver observer) {
         addMonitoredPage(url);
         _observers.putIfAbsent(url, new HashSet<IObserver>());
         _observers.get(url).add(observer);
     }
 
+    /**
+     * @param url      url from which the observer will be dettached
+     * @param observer observer that will be detached from the url
+     */
     public void removeObserver(URL url, IObserver observer) {
         if (_observers.containsKey(url)) {
             _observers.get(url).remove(observer);
