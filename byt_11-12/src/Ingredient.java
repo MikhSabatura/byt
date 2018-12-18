bd5d344be65629541320638c023f4039a32b204f
MikhSabatura <mikhailsabatura@gmail.com>
2018-12-18 16:24:54 +0100

    Added constraints to Ingredient

--- byt_11-12/src/Ingredient.java
+++ byt_11-12/src/Ingredient.java
@@ -1,7 +1,7 @@
 public class Ingredient {
 
-    private String name; // <= 50 symbols
-    private String description; // <= 1000 symbols
+    private String name; // not empty && <= 50 symbols
+    private String description; // not empty && <= 1000 symbols
 
     public Ingredient(String name, String description) {
         setName(name);
@@ -13,8 +13,8 @@ public class Ingredient {
     }
 
     public void setName(String name) {
-        if (name.length() > 50) {
-            throw new IllegalArgumentException("Name should be <= 50 symbols");
+        if (name.length() == 0 || name.length() > 50) {
+            throw new IllegalArgumentException("Name should be <= 50 symbols and not empty");
         }
         this.name = name;
     }
@@ -24,8 +24,8 @@ public class Ingredient {
     }
 
     public void setDescription(String description) {
-        if (description.length() > 1000) {
-            throw new IllegalArgumentException("Description should be <= 1000 symbols");
+        if (description.length() == 0 || description.length() > 1000) {
+            throw new IllegalArgumentException("Description should be <= 1000 symbols and not empty");
         }
         this.description = description;
     }
